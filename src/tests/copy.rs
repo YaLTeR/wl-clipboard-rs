98d641a4bdd144e59a18b2f0a72a6fb24e36d32b
yory8 <>
2019-11-01 20:35:39 +0100

    implement test

--- src/tests/copy.rs
+++ src/tests/copy.rs
@@ -186,6 +186,111 @@ fn copy_test() {
     assert_eq!(contents, [1, 3, 3, 7]);
 }
 
+#[test]
+fn copy_multi_test() {
+    struct ServerManagerHandler {
+        selection: Rc<RefCell<Option<ServerSource>>>,
+    }
+
+    impl ServerManagerRequestHandler for ServerManagerHandler {
+        fn create_data_source(&mut self, _manager: ServerManager, id: NewResource<ServerSource>) {
+            id.implement_closure(|request, source| {
+                                     if let ServerSourceRequest::Offer { mime_type } = request {
+                                         source.as_ref()
+                                               .user_data::<RefCell<Vec<_>>>()
+                                               .unwrap()
+                                               .borrow_mut()
+                                               .push(mime_type);
+                                     }
+                                 },
+                                 None::<fn(_)>,
+                                 RefCell::new(Vec::<String>::new()));
+        }
+
+        fn get_data_device(&mut self,
+                           _manager: ServerManager,
+                           id: NewResource<ServerDevice>,
+                           _seat: ServerSeat) {
+            let selection = self.selection.clone();
+            id.implement_closure(move |request, _| {
+                                     if let ServerDeviceRequest::SetSelection { source } = request {
+                                         *selection.borrow_mut() = source;
+                                     }
+                                 },
+                                 None::<fn(_)>,
+                                 ());
+        }
+    }
+
+    let mut server = TestServer::new();
+    server.display
+          .create_global::<ServerSeat, _>(6, |new_res, _| {
+              new_res.implement_dummy();
+          });
+
+    let selection = Rc::new(RefCell::new(None));
+    {
+        let selection = selection.clone();
+        server.display
+              .create_global::<ServerManager, _>(1, move |new_res, _| {
+                  new_res.implement(ServerManagerHandler { selection: selection.clone() },
+                                    None::<fn(_)>,
+                                    ());
+              });
+    }
+
+    let socket_name = mem::replace(&mut server.socket_name, OsString::new());
+    let child = thread::spawn(move || {
+        let mut opts = Options::new();
+        opts.foreground(true);
+        let sources = vec![MimeSource { source: Source::Bytes(&[1, 3, 3, 7]), mime: MimeType::Specific("test".to_string()) }, MimeSource { source: Source::Bytes(&[2, 4, 4]), mime: MimeType::Specific("test2".to_string()) }];
+        copy_internal(opts,
+                      sources,
+                      Some(socket_name),
+                      true)
+    });
+
+    thread::sleep(Duration::from_millis(100));
+    server.answer();
+
+    thread::sleep(Duration::from_millis(100));
+    server.answer();
+
+    thread::sleep(Duration::from_millis(100));
+    server.answer();
+
+    let mime_types = selection.borrow().as_ref().map(|x| {
+                                                    x.as_ref()
+                                                     .user_data::<RefCell<Vec<String>>>()
+                                                     .unwrap()
+                                                     .borrow()
+                                                     .clone()
+                                                });
+
+    let (mut read, write) = pipe().unwrap();
+
+    if let Some(source) = selection.borrow().as_ref() {
+        source.send("test".to_string(), write.as_raw_fd());
+        source.send("test2".to_string(), write.as_raw_fd());
+        drop(write);
+        source.cancelled();
+    }
+
+    thread::sleep(Duration::from_millis(100));
+    server.answer();
+
+    let mut contents = vec![];
+    read.read_to_end(&mut contents).unwrap();
+
+    child.join().unwrap().unwrap();
+
+    assert!(mime_types.is_some());
+    let mut mimes = mime_types.unwrap();
+    mimes.sort();
+    assert_eq!(mimes, vec!["test".to_string(), "test2".to_string()]);
+    assert_eq!(contents, [1, 3, 3, 7, 2, 4, 4]);
+}
+
 // The idea here is to exceed the pipe capacity. This fails unless O_NONBLOCK is cleared when
 // sending data over the pipe using cat.
 #[test]
