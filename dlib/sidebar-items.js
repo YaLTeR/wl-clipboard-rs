3522f87cce46b6b83c18edcaa0b2293fce0a4c7c
YaLTeR <YaLTeR@users.noreply.github.com>
2022-03-14 08:00:40 +0000

    deploy: e09c0f1a759fbccb15225fb95d7d3a33a8513fe9

---
+++ dlib/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["DlError","An error generated when failing to load a library"],["LibLoadingError","Errors."]],"macro":[["external_library","Main macro of this library, used to generate the the FFI bindings."],["ffi_dispatch","Macro for generically invoking a FFI function"],["ffi_dispatch_static","Macro for generically accessing a FFI static"]]});
