6320cb32eead02258698d5876c538669207d1497
YaLTeR <YaLTeR@users.noreply.github.com>
2022-09-24 02:07:26 +0000

    deploy: e87a04d531eedeac6a44f299351546a7f32d752c

--- source-files.js
+++ source-files.js
@@ -1,39 +1,40 @@
-var sourcesIndex = {};
-sourcesIndex["bitflags"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["bytecount"] = {"name":"","files":["integer_simd.rs","lib.rs","naive.rs"]};
-sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["derive_new"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["downcast_rs"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["fastrand"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["fixedbitset"] = {"name":"","files":["lib.rs","range.rs"]};
-sourcesIndex["fnv"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["hashbrown"] = {"name":"","dirs":[{"name":"external_trait_impls","files":["mod.rs"]},{"name":"raw","files":["alloc.rs","bitmask.rs","mod.rs","sse2.rs"]}],"files":["lib.rs","macros.rs","map.rs","scopeguard.rs","set.rs"]};
-sourcesIndex["indexmap"] = {"name":"","dirs":[{"name":"map","dirs":[{"name":"core","files":["raw.rs"]}],"files":["core.rs"]}],"files":["equivalent.rs","lib.rs","macros.rs","map.rs","mutable_keys.rs","set.rs","util.rs"]};
-sourcesIndex["lazy_static"] = {"name":"","files":["inline_lazy.rs","lib.rs"]};
-sourcesIndex["libc"] = {"name":"","dirs":[{"name":"unix","dirs":[{"name":"linux_like","dirs":[{"name":"linux","dirs":[{"name":"arch","dirs":[{"name":"generic","files":["mod.rs"]}],"files":["mod.rs"]},{"name":"gnu","dirs":[{"name":"b64","dirs":[{"name":"x86_64","files":["align.rs","mod.rs","not_x32.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["align.rs","mod.rs","non_exhaustive.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
-sourcesIndex["log"] = {"name":"","files":["lib.rs","macros.rs"]};
-sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"memchr","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","mod.rs","naive.rs"]},{"name":"memmem","dirs":[{"name":"prefilter","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse.rs"]}],"files":["fallback.rs","genericsimd.rs","mod.rs"]},{"name":"x86","files":["avx.rs","mod.rs","sse.rs"]}],"files":["byte_frequencies.rs","genericsimd.rs","mod.rs","rabinkarp.rs","rarebytes.rs","twoway.rs","util.rs","vector.rs"]}],"files":["cow.rs","lib.rs"]};
-sourcesIndex["memoffset"] = {"name":"","files":["lib.rs","offset_of.rs","raw_field.rs","span_of.rs"]};
-sourcesIndex["minimal_lexical"] = {"name":"","files":["bigint.rs","extended_float.rs","lemire.rs","lib.rs","mask.rs","num.rs","number.rs","parse.rs","rounding.rs","slow.rs","stackvec.rs","table.rs","table_lemire.rs","table_small.rs"]};
-sourcesIndex["nix"] = {"name":"","dirs":[{"name":"mount","files":["linux.rs","mod.rs"]},{"name":"net","files":["if_.rs","mod.rs"]},{"name":"sys","dirs":[{"name":"ioctl","files":["linux.rs","mod.rs"]},{"name":"ptrace","files":["linux.rs","mod.rs"]},{"name":"socket","files":["addr.rs","mod.rs","sockopt.rs"]}],"files":["aio.rs","epoll.rs","eventfd.rs","inotify.rs","memfd.rs","mman.rs","mod.rs","personality.rs","pthread.rs","quota.rs","reboot.rs","resource.rs","select.rs","sendfile.rs","signal.rs","signalfd.rs","stat.rs","statfs.rs","statvfs.rs","sysinfo.rs","termios.rs","time.rs","timer.rs","timerfd.rs","uio.rs","utsname.rs","wait.rs"]}],"files":["dir.rs","env.rs","errno.rs","fcntl.rs","features.rs","ifaddrs.rs","kmod.rs","lib.rs","macros.rs","mqueue.rs","poll.rs","pty.rs","sched.rs","time.rs","ucontext.rs","unistd.rs"]};
-sourcesIndex["nom"] = {"name":"","dirs":[{"name":"bits","files":["complete.rs","mod.rs","streaming.rs"]},{"name":"branch","files":["mod.rs"]},{"name":"bytes","files":["complete.rs","mod.rs","streaming.rs"]},{"name":"character","files":["complete.rs","mod.rs","streaming.rs"]},{"name":"combinator","files":["mod.rs"]},{"name":"multi","files":["mod.rs"]},{"name":"number","files":["complete.rs","mod.rs","streaming.rs"]},{"name":"sequence","files":["mod.rs"]}],"files":["error.rs","internal.rs","lib.rs","str.rs","traits.rs"]};
-sourcesIndex["once_cell"] = {"name":"","files":["imp_std.rs","lib.rs","race.rs"]};
-sourcesIndex["os_pipe"] = {"name":"","files":["lib.rs","unix.rs"]};
-sourcesIndex["petgraph"] = {"name":"","dirs":[{"name":"algo","files":["astar.rs","bellman_ford.rs","dijkstra.rs","dominators.rs","feedback_arc_set.rs","floyd_warshall.rs","isomorphism.rs","k_shortest_path.rs","matching.rs","mod.rs","simple_paths.rs","tred.rs"]},{"name":"graph_impl","dirs":[{"name":"stable_graph","files":["mod.rs"]}],"files":["frozen.rs","mod.rs"]},{"name":"visit","files":["dfsvisit.rs","filter.rs","macros.rs","mod.rs","reversed.rs","traversal.rs"]}],"files":["adj.rs","csr.rs","data.rs","dot.rs","graphmap.rs","iter_format.rs","iter_utils.rs","lib.rs","macros.rs","matrix_graph.rs","operator.rs","prelude.rs","scored.rs","traits_graph.rs","unionfind.rs","util.rs"]};
-sourcesIndex["proc_macro2"] = {"name":"","files":["detection.rs","fallback.rs","lib.rs","marker.rs","parse.rs","rcvec.rs","wrapper.rs"]};
-sourcesIndex["quote"] = {"name":"","files":["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]};
-sourcesIndex["remove_dir_all"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["smallvec"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","files":["clone.rs","gen_helper.rs"]}],"files":["attr.rs","await.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","error.rs","export.rs","expr.rs","ext.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]};
-sourcesIndex["tempfile"] = {"name":"","dirs":[{"name":"file","dirs":[{"name":"imp","files":["mod.rs","unix.rs"]}],"files":["mod.rs"]}],"files":["dir.rs","error.rs","lib.rs","spooled.rs","util.rs"]};
-sourcesIndex["thiserror"] = {"name":"","files":["aserror.rs","display.rs","lib.rs"]};
-sourcesIndex["thiserror_impl"] = {"name":"","files":["ast.rs","attr.rs","expand.rs","fmt.rs","generics.rs","lib.rs","prop.rs","valid.rs"]};
-sourcesIndex["tree_magic_mini"] = {"name":"","dirs":[{"name":"basetype","files":["check.rs","init.rs","mod.rs"]},{"name":"fdo_magic","dirs":[{"name":"builtin","files":["check.rs","init.rs","mod.rs","runtime.rs"]}],"files":["check.rs","mod.rs","ruleset.rs"]}],"files":["lib.rs"]};
-sourcesIndex["unicode_ident"] = {"name":"","files":["lib.rs","tables.rs"]};
-sourcesIndex["wayland_client"] = {"name":"","dirs":[{"name":"rust_imp","files":["connection.rs","display.rs","mod.rs","proxy.rs","queues.rs"]}],"files":["display.rs","event_queue.rs","globals.rs","lib.rs","proxy.rs"]};
-sourcesIndex["wayland_commons"] = {"name":"","files":["debug.rs","filter.rs","lib.rs","map.rs","socket.rs","user_data.rs","wire.rs"]};
-sourcesIndex["wayland_protocols"] = {"name":"","files":["lib.rs","misc.rs","protocol_macro.rs","stable.rs","unstable.rs","wlr.rs"]};
-sourcesIndex["wayland_server"] = {"name":"","dirs":[{"name":"rust_imp","files":["clients.rs","display.rs","event_loop_glue.rs","globals.rs","mod.rs","resources.rs"]}],"files":["client.rs","display.rs","globals.rs","lib.rs","resource.rs"]};
-sourcesIndex["wayland_sys"] = {"name":"","files":["client.rs","common.rs","lib.rs","server.rs"]};
-sourcesIndex["wl_clipboard_rs"] = {"name":"","files":["common.rs","copy.rs","handlers.rs","lib.rs","paste.rs","seat_data.rs","utils.rs"]};
+var sourcesIndex = JSON.parse('{\
+"bitflags":["",[],["lib.rs"]],\
+"bytecount":["",[],["integer_simd.rs","lib.rs","naive.rs"]],\
+"cfg_if":["",[],["lib.rs"]],\
+"derive_new":["",[],["lib.rs"]],\
+"downcast_rs":["",[],["lib.rs"]],\
+"fastrand":["",[],["lib.rs"]],\
+"fixedbitset":["",[],["lib.rs","range.rs"]],\
+"fnv":["",[],["lib.rs"]],\
+"hashbrown":["",[["external_trait_impls",[],["mod.rs"]],["raw",[],["alloc.rs","bitmask.rs","mod.rs","sse2.rs"]]],["lib.rs","macros.rs","map.rs","scopeguard.rs","set.rs"]],\
+"indexmap":["",[["map",[["core",[],["raw.rs"]]],["core.rs"]]],["equivalent.rs","lib.rs","macros.rs","map.rs","mutable_keys.rs","set.rs","util.rs"]],\
+"lazy_static":["",[],["inline_lazy.rs","lib.rs"]],\
+"libc":["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["align.rs","mod.rs","not_x32.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["align.rs","mod.rs","non_exhaustive.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]],\
+"log":["",[],["lib.rs","macros.rs"]],\
+"memchr":["",[["memchr",[["x86",[],["avx.rs","mod.rs","sse2.rs"]]],["fallback.rs","iter.rs","mod.rs","naive.rs"]],["memmem",[["prefilter",[["x86",[],["avx.rs","mod.rs","sse.rs"]]],["fallback.rs","genericsimd.rs","mod.rs"]],["x86",[],["avx.rs","mod.rs","sse.rs"]]],["byte_frequencies.rs","genericsimd.rs","mod.rs","rabinkarp.rs","rarebytes.rs","twoway.rs","util.rs","vector.rs"]]],["cow.rs","lib.rs"]],\
+"memoffset":["",[],["lib.rs","offset_of.rs","raw_field.rs","span_of.rs"]],\
+"minimal_lexical":["",[],["bigint.rs","extended_float.rs","lemire.rs","lib.rs","mask.rs","num.rs","number.rs","parse.rs","rounding.rs","slow.rs","stackvec.rs","table.rs","table_lemire.rs","table_small.rs"]],\
+"nix":["",[["mount",[],["linux.rs","mod.rs"]],["net",[],["if_.rs","mod.rs"]],["sys",[["ioctl",[],["linux.rs","mod.rs"]],["ptrace",[],["linux.rs","mod.rs"]],["socket",[],["addr.rs","mod.rs","sockopt.rs"]]],["aio.rs","epoll.rs","eventfd.rs","inotify.rs","memfd.rs","mman.rs","mod.rs","personality.rs","pthread.rs","quota.rs","reboot.rs","resource.rs","select.rs","sendfile.rs","signal.rs","signalfd.rs","stat.rs","statfs.rs","statvfs.rs","sysinfo.rs","termios.rs","time.rs","timer.rs","timerfd.rs","uio.rs","utsname.rs","wait.rs"]]],["dir.rs","env.rs","errno.rs","fcntl.rs","features.rs","ifaddrs.rs","kmod.rs","lib.rs","macros.rs","mqueue.rs","poll.rs","pty.rs","sched.rs","time.rs","ucontext.rs","unistd.rs"]],\
+"nom":["",[["bits",[],["complete.rs","mod.rs","streaming.rs"]],["branch",[],["mod.rs"]],["bytes",[],["complete.rs","mod.rs","streaming.rs"]],["character",[],["complete.rs","mod.rs","streaming.rs"]],["combinator",[],["mod.rs"]],["multi",[],["mod.rs"]],["number",[],["complete.rs","mod.rs","streaming.rs"]],["sequence",[],["mod.rs"]]],["error.rs","internal.rs","lib.rs","str.rs","traits.rs"]],\
+"once_cell":["",[],["imp_std.rs","lib.rs","race.rs"]],\
+"os_pipe":["",[],["lib.rs","unix.rs"]],\
+"petgraph":["",[["algo",[],["astar.rs","bellman_ford.rs","dijkstra.rs","dominators.rs","feedback_arc_set.rs","floyd_warshall.rs","isomorphism.rs","k_shortest_path.rs","matching.rs","mod.rs","simple_paths.rs","tred.rs"]],["graph_impl",[["stable_graph",[],["mod.rs"]]],["frozen.rs","mod.rs"]],["visit",[],["dfsvisit.rs","filter.rs","macros.rs","mod.rs","reversed.rs","traversal.rs"]]],["adj.rs","csr.rs","data.rs","dot.rs","graphmap.rs","iter_format.rs","iter_utils.rs","lib.rs","macros.rs","matrix_graph.rs","operator.rs","prelude.rs","scored.rs","traits_graph.rs","unionfind.rs","util.rs"]],\
+"proc_macro2":["",[],["detection.rs","fallback.rs","lib.rs","marker.rs","parse.rs","rcvec.rs","wrapper.rs"]],\
+"quote":["",[],["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]],\
+"remove_dir_all":["",[],["lib.rs"]],\
+"smallvec":["",[],["lib.rs"]],\
+"syn":["",[["gen",[],["clone.rs","gen_helper.rs"]]],["attr.rs","await.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","error.rs","export.rs","expr.rs","ext.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]],\
+"tempfile":["",[["file",[["imp",[],["mod.rs","unix.rs"]]],["mod.rs"]]],["dir.rs","error.rs","lib.rs","spooled.rs","util.rs"]],\
+"thiserror":["",[],["aserror.rs","display.rs","lib.rs"]],\
+"thiserror_impl":["",[],["ast.rs","attr.rs","expand.rs","fmt.rs","generics.rs","lib.rs","prop.rs","valid.rs"]],\
+"tree_magic_mini":["",[["basetype",[],["check.rs","init.rs","mod.rs"]],["fdo_magic",[["builtin",[],["check.rs","init.rs","mod.rs","runtime.rs"]]],["check.rs","mod.rs","ruleset.rs"]]],["lib.rs"]],\
+"unicode_ident":["",[],["lib.rs","tables.rs"]],\
+"wayland_client":["",[["rust_imp",[],["connection.rs","display.rs","mod.rs","proxy.rs","queues.rs"]]],["display.rs","event_queue.rs","globals.rs","lib.rs","proxy.rs"]],\
+"wayland_commons":["",[],["debug.rs","filter.rs","lib.rs","map.rs","socket.rs","user_data.rs","wire.rs"]],\
+"wayland_protocols":["",[],["lib.rs","misc.rs","protocol_macro.rs","stable.rs","unstable.rs","wlr.rs"]],\
+"wayland_server":["",[["rust_imp",[],["clients.rs","display.rs","event_loop_glue.rs","globals.rs","mod.rs","resources.rs"]]],["client.rs","display.rs","globals.rs","lib.rs","resource.rs"]],\
+"wayland_sys":["",[],["client.rs","common.rs","lib.rs","server.rs"]],\
+"wl_clipboard_rs":["",[],["common.rs","copy.rs","handlers.rs","lib.rs","paste.rs","seat_data.rs","utils.rs"]]\
+}');
 createSourceSidebar();
